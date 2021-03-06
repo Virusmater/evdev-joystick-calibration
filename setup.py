e80e17b4dd532e604b822fc99d73cf965d77a291
Virusmater <virusmater@gmail.com>
2021-03-06 13:18:43 +0100

    force evdev version 1.3.0 and above

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='evdev-joystick-calibration',
-    version='0.1',
+    version='0.1.1',
     description='Run, pick up the gamepad and turn sticks with triggers around',
     url='https://github.com/Virusmater/evdev-joystick-calibration',
     author='Dima Kompot',
     author_email='virusmater@gmail.com',
     license='MIT',
-    install_requires=['evdev'],
+    install_requires=['evdev>=1.3.0'],
     packages=find_packages(),
     entry_points=dict(
         console_scripts=['evdev-joystick-calibration=evdev_joystick_calibration.__main__:main']
